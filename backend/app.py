c0dc3e416669bdc956aa7858577246a7c34349a7
Owen-Staddon-CB <owen.staddon@coldbanana.com>
2023-11-17 20:33:15 +0000

    adds csv reader

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -6,7 +7,16 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-
+@app.route('/jsonParser', methods=['GET'])
+def parseJson():
+    if request.method == 'GET':
+        file_path = 'emptyJson.json'# you will need to change this to your json file name
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as file:
+                json_data = file.read()
+                return json_data
+        else:
+            return jsonify({'error': 'File not found'}), 404 
 
 @app.route('/getData', methods=['GET'])
 def getData():
@@ -18,7 +28,7 @@ def getData():
                 ]
             }
         } 
-  
+        print(data)
         return jsonify(data)
     
 if __name__ == '__main__':
