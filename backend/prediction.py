aa0a98f301730f1891641c556d11f92a813a1e89
nizar.naasani <nizar.naasani@coldbanana.com>
2023-11-18 17:12:35 +0400

    Add Api call

---
+++ backend/prediction.py
@@ -0,0 +1,18 @@
+import pickle
+import pandas as pd
+import json
+
+def predict_disease(config):
+    ##loading the model from the saved file
+    pkl_filename = "model.pkl"
+    with open(pkl_filename, 'rb') as f_in:
+        model = pickle.load(f_in)
+
+    if type(config) == dict:
+        df = pd.DataFrame(config)
+    else:
+        df = config
+    
+    prognosis = model.predict(df)
+    
+    return prognosis
